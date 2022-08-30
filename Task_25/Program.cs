9db05f28028d949e5fd0dbd62011624304449c3b
Darya <darecc@yandex.ru>
2022-08-30 21:16:27 +0400

    add Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число A: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("-> " + GetDegree(num));
+
+double GetDegree(int number)
+{
+    double result = Math.Pow(num, num1);
+    
+    return result;
+}
