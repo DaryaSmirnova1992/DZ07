fc58909e62b076df2cd550649f91fa31da501cdd
Darya <darecc@yandex.ru>
2022-08-30 22:30:31 +0400

    add Task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,57 @@
+﻿
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("-> "+GetSum(num));
+
+// int GetCount(int number)
+// {
+//  int count =0;
+//  while(number>0)
+//  {
+// 		count++;
+// 		number/=10;
+//  }
+// 	return count;
+
+// }
+// int quantity = GetCount(num);
+
+// int GetSum(int number)
+// {
+//     int sum = 0;
+//     for(int i = 0; i <= quantity; i++)
+//     {
+//         int num1 = number % 10;
+//         number = number / 10;
+//         sum = sum + num1;
+//     }
+//         return sum;
+    
+// }
+
+
+
+
+
+
+
+
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("-> "+GetSum(num));
+
+int GetSum(int number)
+{
+ int count =0;
+ int sum =0;
+ while(number>0)
+ {
+    int num1 = number % 10;
+     sum = sum + num1;
+		count++;
+		number/=10;
+ }
+	return sum;
+
+}
